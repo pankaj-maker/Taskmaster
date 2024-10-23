9f5aec314dc6844f38caa6c612d2038661433b46
pankaj-maker <Pankaj14321@protonmail.com>
2024-10-23 07:12:15 +0000

    footer added

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
 import "./index.css"
-console.log('hi');
+// console.log('hi');
+
+const index=[1,2,3,4,5]
+function getRandom(arry){
+    const list=[...arry]
+    return ()=>{
+        const rn=Math.floor(Math.random()*list.length);
+        const removed=list.splice(rn,1)[0]
+        return removed
+    }
+    
+}
+console.log(getRandom());
+
