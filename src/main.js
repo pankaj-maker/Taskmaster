8ba5408c83cbd3c6b39b25657c75e22bb16eac64
pankaj-maker <Pankaj14321@protonmail.com>
2024-10-28 08:04:54 +0000

    show task

--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
 import "./index.css"
-// console.log('hi');
+import { titleCase } from "./utils";
+const formEl=document.querySelector("[data-form]");
+const inputEl=document.querySelector("[data-user-input]")
+const task=[];
 
-const index=[1,2,3,4,5]
-function getRandom(arry){
-    const list=[...arry]
-    return ()=>{
-        const rn=Math.floor(Math.random()*list.length);
-        const removed=list.splice(rn,1)[0]
-        return removed
-    }
+formEl.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    if(!inputEl.value) return;
+    const newTask=task.push({
+        text: titleCase(inputEl.value),
+        isCompleted:false,
+        id:task.length,
+})
+    task.unshift(newTask)
+    console.log(task)
+    inputEl.value="";
     
-}
-console.log(getRandom());
+})
 
+
+
+const showYearEl=document.querySelector(".show-year")
+showYearEl.textContent=new Date().getFullYear();
