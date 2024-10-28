d93dd2d3a9591aec1008473a943fe12585ce4de2
pankaj-maker <Pankaj14321@protonmail.com>
2024-10-28 07:33:14 +0000

    created new task

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {},
+    fontFamily:{
+     'figtree': ["figtree","sans-serif"]
+    },
   },
   plugins: [require('daisyui'),],
   daisyui: {
