f1ca8489ec6a1cfa787276607ddc49095337a904
pankaj-maker <Pankaj14321@protonmail.com>
2024-10-29 08:06:26 +0000

    teri marzi

--- src/components/SingleTask.js
+++ src/components/SingleTask.js
@@ -1,12 +1,16 @@
-export default function(text,isCompleted){
-
-    const label=document.createElement("label");
-    label.classList.add("label","cursor-pointer")
-    if(isCompleted){
-        label.classList.add("bg-slate-300" ,"rounded" ,"p-2")
+export default function (text, isCompleted,id) {
+    const label = document.createElement("label");
+    label.classList.add("label", "cursor-pointer");
+  
+    if (isCompleted) {
+      label.classList.add("bg-slate-200", "rounded", "p-2");
     }
-        label.innerHTML=   `<span class="label-text">${text}</span>
-              <input type="checkbox" ${isCompleted && "checked"}class="checkbox checkbox-primary" />`
-    
+  
+    label.innerHTML = `
+      <span class="label-text">${text}</span>
+      <input type="checkbox" ${
+        isCompleted && "checked"
+      } class="checkbox checkbox-primary"id=${id} />
+    `;
     return label;
-}
+  }
--- src/main.js
+++ src/main.js
@@ -1,35 +1,68 @@
-import "./index.css"
-import { titleCase } from "./utils";
+import "./index.css";
+
 import SingleTask from "./components/SingleTask";
-const formEl=document.querySelector("[data-form]");
-const inputEl=document.querySelector("[data-user-input]")
-const taskContainerEl=document.querySelector("[data-task-container]")
-const task=[];
-function renderTask(){
-    taskContainerEl.innerText="";
-    const frag=document.createDocumentFragment();
-    task.forEach((task)=>{
-        frag.appendChild(SingleTask(task.text,task.isCompleted))
+import { titleCase,randomId } from "./utils";
+
+// === MARK: DOM Selection
+const formEl = document.querySelector("[data-form]");
+const inputEl = document.querySelector("[data-user-input]");
+const taskContainerEl = document.querySelector("[data-task-container]");
+
+// Variables
+let state = [];
+
+function toggleCompleted(id){
+   state =state.map((task)=>{
+        if(id===task.id){
+            return{...task,isCompleted: !task.isCompleted}
+        }
+        return task;
     })
-    taskContainerEl.appendChild(frag)
+ 
+ console.log(state);
+ 
+    
+}
+function renderTasks() {
+  taskContainerEl.innerHTML = "";
+
+  const frag = document.createDocumentFragment();
+  state.forEach((task) => {
+    frag.appendChild(SingleTask(task.text, task.isCompleted,task.id));
+  });
+  taskContainerEl.appendChild(frag);
 }
 
-formEl.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    if(!inputEl.value) return;
-    const newTask=task.push({
-        text: titleCase(inputEl.value),
-        isCompleted:false,
-        id:task.length,
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault(); // Prevent refresh
+  if (!inputEl.value) return; // Gaurd Clause
+
+  //  Creating new task
+  const newTask = {
+    text: titleCase(inputEl.value),
+    isCompleted:false,
+    id: randomId(),
+  };
+
+  //  Adding
+  state.unshift(newTask);
+
+  renderTasks();
+
+  console.log(state);
+
+  //  Clearing input value
+  inputEl.value = "";
+});
+
+// Render the current year
+const showYearEl = document.querySelector(".show-year");
+showYearEl.textContent = new Date().getFullYear();
+
+
+taskContainerEl.addEventListener("click",(e)=>{
+    if(e.target.tagName==="INPUT"){
+        toggleCompleted(e.target.id);
+        renderTasks();
+        }
 })
-    task.unshift(newTask)
-    renderTask();
-    console.log(task)
-    inputEl.value="";
-    
-})
-
-
-
-const showYearEl=document.querySelector(".show-year")
-showYearEl.textContent=new Date().getFullYear();
